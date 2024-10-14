85ec4719ba45f37143f977f5a802c4b178e44e60
Cheesidibbl <ztech.z@icloud.com>
2024-10-14 09:04:01 -0700

    Update server.js

--- client/server.js
+++ client/server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the main HTML file
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/chat.html'); // Ensure you have an index.html file
+    res.sendFile(path.join(__dirname, 'public', 'chat.html'));
 });
 
 io.on('connection', (socket) => {
