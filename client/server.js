3000028410a8f66cbcef9aa6ba17978d8ff9ec6a
Cheesidibbl <ztech.z@icloud.com>
2024-10-13 20:39:27 -0700

    Update server.js

--- client/server.js
+++ client/server.js
@@ -22,7 +22,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
